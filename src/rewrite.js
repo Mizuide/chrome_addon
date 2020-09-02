2c1e44b056c4fad37a7f276910323c467d200a23
m <waterexit1120@gmail.com>
2020-09-02 10:45:24 +0900

    GET通信送信処理を整理

--- src/rewrite.js
+++ src/rewrite.js
@@ -1,23 +1,39 @@
-  import axios  from "axios";
-
-
-        let page = 2;
-        let magazineId = 'm53c1789fa592';
+import axios from "axios";
 
 let fet = axios.create({
-  baseURL:'https://note.com/api/'
+  baseURL: 'https://note.com/api/'
 })
-        const API_URI = 'https://note.com/api/v1/layout/magazine/'+magazineId+'/section?page='+page+'&include_details=true'
 
 
-       fet.get('v1/layout/magazine/'+magazineId+'/section?page='+page+'&include_details=true').then(res => console.log(res)).catch(e => console.log(e))
+let page = 2;
+let magazineId = specificMagazine();
 
-        //document.body.style.backgroundColor = "blue";
-          var divList = document.querySelectorAll("div");
-          divList.forEach(function(div,index){div.outerHTML="い"})
+let API_GET = function(){
+   return 'v1/layout/magazine/' + magazineId + '/section?page=' + page + '&include_details=true' 
+  }
 
+fetchAPI().then(res => setMaxCount(res)).then(() => fetchAPI()).then(res => console.log(res));
 
+//document.body.style.backgroundColor = "blue";
+// var divList = document.querySelectorAll("div");
+// divList.forEach(function (div, index) { div.outerHTML = "い" });
 
-        function fetchAPI(){
-          
-        }
+function specificMagazine() {
+  let path = location.pathname;
+  let idString = path.split('/')[3];
+  return idString;
+}
+
+function fetchAPI() {
+  console.log('get通信')
+  console.log(API_GET());
+  return fet.get(API_GET()).catch(e => console.log(e));
+}
+
+function setMaxCount(response) {
+// return new Promise(function(resolve,reject){
+  let maxCount = response.data.data.section.total_count;
+  page = maxCount;
+  // resolve(maxCount);
+// })
+}
